8134a64f7b60deb065b81d358750cc4deb8243ce
nqobilekhani <3021128@student.rnngroup.ac.uk>
2026-02-24 19:41:34 +0000

    First Commit changed message

---
+++ nqobile_password_generator.py
@@ -0,0 +1,37 @@
+# random password generator
+# Generates a random password containing a mix of characters.
+
+import sys
+import random
+import string
+
+def generate_password(length=12):
+    """
+    Generates a random password containing a mix of characters.
+    """
+
+    # Define the sets of characters to use
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+    # Ensure the length is a positive integer
+    if length <= 0:
+      return "Password length must be greater than 0."
+
+    # Generate the password by randomly sampling from the character set
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+
+if len(sys.argv) < 2:
+  print("-----This is a simple passowrd generator-----")
+  size =int(input("Enter password length:  "))
+
+else:
+  size = int(sys.argv[1])
+# Print the password to the screen
+# password_12 = generate_password()
+# print(f"12-Character Password: {password_12}")
+
+new_password = generate_password(size)
+print(f"Your new password is: {new_password}")
